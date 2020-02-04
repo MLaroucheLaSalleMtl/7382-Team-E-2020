d7d5cc84050f0e72384c877245f3847ef097a387
aug551 <aug551@github.com>
2020-02-04 16:08:53 -0500

    First commit with Unity files.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private Vector2 move = new Vector2();
+    private Vector3 zeroVelocity = Vector3.zero;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float smoothing = 0.1f;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = new Vector3();
+        pos.x = move.x * speed;
+        pos.y = move.y * speed;
+        rigid.velocity = Vector3.SmoothDamp(rigid.velocity, pos, ref zeroVelocity, smoothing);
+    }
+}
