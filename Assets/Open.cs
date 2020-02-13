355efddc90b93e662f49ddb75a72f758079cabcc
pixelparrot-88 <59895675+pixelparrot-88@users.noreply.github.com>
2020-02-13 18:12:36 -0500

    finalcolliders

---
+++ Assets/Open.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    public Animator OpenDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenDoor = GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            OpenDoor.SetBool("Open",true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            OpenDoor.SetBool("Open", false);
+        }
+    }
+}
