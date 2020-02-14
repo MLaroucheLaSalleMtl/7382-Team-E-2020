40d62721d2b0ff862339520b96c0f5df98d8aef5
pixelparrot-88 <59895675+pixelparrot-88@users.noreply.github.com>
2020-02-14 15:55:52 -0500

    Slidedoors

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,38 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    public Animator animator;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            animator.SetBool("open", true);
+            Invoke("DestroyObject", 1.5f);
+        }
+    }
+
+    private void DestroyObject()
+    {
+        this.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
