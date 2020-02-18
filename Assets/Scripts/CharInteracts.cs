456e47e39c3ce3a7a9893eb1c9f03f3c569fd04b
aug551 <aug551@github.com>
2020-02-18 15:50:11 -0500

    added aiming for up angles

--- Assets/Scripts/CharInteracts.cs
+++ Assets/Scripts/CharInteracts.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 
 public class CharInteracts : MonoBehaviour
 {
+    [SerializeField] Transform cross;
+    [SerializeField] Sprite[] sprites;
     private bool fire = false;
+
     public void OnShoot(InputAction.CallbackContext context)
     {
         Debug.Log(context.ReadValue<float>() > 0);
@@ -17,9 +20,31 @@ public class CharInteracts : MonoBehaviour
         
     }
 
+
+    // tan x = OP/Adj
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log(fire);
+        
+        float radians = cross.localPosition.y/cross.localPosition.x;
+        float angle = radians * (180/Mathf.PI);
+        Debug.Log(angle);
+        
+        if(Mathf.Abs(angle) < 20){
+        this.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+        else if(Mathf.Abs(angle) > 20 && Mathf.Abs(angle) < 40)
+        {
+        this.GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
+        else if(Mathf.Abs(angle) > 40 && Mathf.Abs(angle) < 60)
+        {
+        this.GetComponent<SpriteRenderer>().sprite = sprites[2];
+        }
+        else if(Mathf.Abs(angle) > 60)
+        {
+        this.GetComponent<SpriteRenderer>().sprite = sprites[3];
+        }
+
     }
 }
